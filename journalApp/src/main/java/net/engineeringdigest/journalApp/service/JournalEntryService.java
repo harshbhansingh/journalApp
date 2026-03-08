75cdf7ca7c90428294226021bd5e9d4d695d6176
Harsh <harshbhan26@gmail.com>
2026-03-08 13:50:58 +0530

    Basic API Structure I have added , Basic CRUD Operations

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -0,0 +1,34 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.JournalEntry;
+import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class JournalEntryService {
+
+    @Autowired
+    JournalEntryRepository journalEntryRepository;
+
+    public void saveEntry(JournalEntry journalEntry){
+        journalEntryRepository.save(journalEntry);
+    }
+
+    public List<JournalEntry> getAllEntries(){
+        return journalEntryRepository.findAll();
+    }
+
+    public Optional<JournalEntry> getJournalEntryById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+
+    public void deleteJournalById(ObjectId id){
+        journalEntryRepository.deleteById(id);
+    }
+
+}
